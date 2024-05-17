e5fc2e1d694de756788106b5ae49814b6f7c97cd
swxctx <swxctx@gmail.com>
2024-05-17 15:27:43 +0800

    update server logic

--- server.go
+++ server.go
@@ -1,9 +1,9 @@
 package plex
 
 import (
-	"fmt"
-	"github.com/swxctx/plex/plog"
 	"net"
+
+	"github.com/swxctx/plex/plog"
 )
 
 var (
@@ -19,11 +19,11 @@ type plexServer struct {
 	// conn store cache
 	store *connStore
 	// tcp auth func
-	authFunc func(body string) bool
+	authFunc func(body string) (bool, string)
 }
 
-// NewServer
-func NewServer(config *Config, fn ...func(body string) bool) {
+// Start
+func Start(config *Config, fn ...func(body string) (bool, string)) {
 	// reload config
 	cfg := reloadConfig(config)
 
@@ -40,41 +40,19 @@ func NewServer(config *Config, fn ...func(body string) bool) {
 		server.authFunc = fn[0]
 	}
 	plog.Infof("new plex server success.")
+
+	plog.Infof("--- server start begin ---")
+
+	// start http
+	go server.startHttpServer()
+
+	// start tcp
+	server.startTcpServer()
+
+	plog.Infof("--- server start end ---")
 }
 
-// Start
-func Start() error {
-	plog.Infof("start plex server.")
-
-	if server.authFunc == nil {
-		return fmt.Errorf("plex auth func is nil")
-	}
-
-	// tcp listen
-	listener, err := net.Listen("tcp", fmt.Sprintf(":%s", server.cfg.Port))
-	if err != nil {
-		plog.Errorf("plex tcp server listen err-> %v", err)
-		return fmt.Errorf("plex listen err-> %v", err)
-	}
-	defer listener.Close()
-
-	plog.Infof("plex server is starting...")
-	plog.Infof("start accept job.")
-
-	for {
-		// listen and accept
-		conn, err := listener.Accept()
-		if err != nil {
-			plog.Errorf("listener accept err-> %v", err)
-			continue
-		}
-
-		// start conn logic
-		server.newPlexConnection(conn)
-	}
-}
-
-// SetAuthFunc
-func SetAuthFunc(fn func(body string) bool) {
+// SetAuthFunc func set outer auth func
+func SetAuthFunc(fn func(body string) (bool, string)) {
 	server.authFunc = fn
 }
